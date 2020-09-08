88b0ac29fdf39a42605e8aec424e775eb11139e2
Aleksei Kurilkin <kurilkin.a@yandex.ru>
2020-09-09 01:56:56 +0300

    some classes

---
+++ Doge/Doge/Model/BranchNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doge.Model
+{
+    public class BranchNumber
+    {
+        public int Ip;
+        public int Iq;
+        public int Np;
+
+        public BranchNumber(int ip,int iq, int np)
+        {
+            Ip = ip;
+            Iq = iq;
+            Np = np;
+        }
+
+        public override string ToString()
+        {
+            return $@"ip={Ip}&iq={Iq}&np={Np}";
+        }
+    }
+}
---
+++ Doge/Doge/Model/NodeNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doge.Model
+{
+    public class NodeNumber
+    {
+        public int Ny;
+
+        public NodeNumber(int ny)
+        {
+            Ny = ny;
+        }
+
+        public override string ToString()
+        {
+            return $@"ny={Ny}";
+        }
+    }
+}
